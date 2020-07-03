dafce23736b327f3a28fd4d56347303e6f38f779
Matthew Peters <matthew.peters.nyc@gmail.com>
2020-07-03 16:18:59 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,28 @@
+def using_concat(array_one, array_two)
+  array_one[].concat(array_two)
+end
+
+
+def using_insert(array, num1, num2)
+array.insert(num1, num2)
+end
+
+
+def using_uniq(array)
+
+end
+
+
+def using_flatten(array)
+
+end
+
+
+def using_delete(array)
+
+end
+
+
+def using_delete_at(array)
+
+end
